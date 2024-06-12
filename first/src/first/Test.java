26a248d557ff1d8a8e176dc318b3ea41f75cd8a3
sks05194 <sks05194@gmail.com>
2024-06-12 18:18:07 +0900

    0612 저장

--- first/src/first/Test.java
+++ first/src/first/Test.java
@@ -1,38 +1,36 @@
 package first;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Test {
-	// 출력 코드 길게 쓰기 귀찮아서 만든 메서드1
+	// 출력 대체코드
 	public static void println(Object obj) {
 		System.out.println(obj);
 	}
 
-	// 출력 코드 길게 쓰기 귀찮아서 만든 메서드2
 	public static void print(Object obj) {
 		System.out.print(obj);
 	}
 
-	// 출력 코드 길게 쓰기 귀찮아서 만든 메서드3
 	public static void println() {
 		System.out.println();
 	}
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random r = new Random();
 		
-		
+		print("0 이상 100 이하의 정수를 입력해주세요.");
 	}
 }
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+
+
+
+
+
+
+
+
