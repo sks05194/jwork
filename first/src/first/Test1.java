93644704998d17c2a7822f88cc982b032a3697c9
sks05194 <sks05194@gmail.com>
2024-06-13 14:26:26 +0900

    0613 중간저장

--- first/src/first/Test1.java
+++ first/src/first/Test1.java
@@ -1,6 +1,8 @@
 package first;
 
-public class Test {
+import java.util.Arrays;
+
+public class Test1 {
 	// 출력 대체코드
 	public static void println(Object obj) {
 		System.out.println(obj);
@@ -15,6 +17,15 @@ public class Test {
 	}
 
 	public static void main(String[] args) {
-		
+		int[] array = new int[] { 3, 4, 5, 6, 7 };
+		println(array);
+
+		// for 콜론문
+		for (int k : array)
+			print(k + " ");
+		println();
+
+		// Arrays 이용
+		println(Arrays.toString(array));
 	}
 }
