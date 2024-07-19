7df5ace11aa92c5dcab4388d1bfd27e9473258dc
sks05194 <sks05194@gmail.com>
2024-07-19 15:20:44 +0900

    0719 singleton error fix

--- jdbc_project1/src/miniProject/StudentDAO.java
+++ jdbc_project1/src/miniProject/StudentDAO.java
@@ -1,5 +1,32 @@
 package miniProject;
 
-public class StudentDAO {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class StudentDAO {
+	// 로그인 구현
+	public void loginStudent(String sid, String spw) throws SQLException {
+		String sql = "SELECT * FROM student WHERE sid = '?', spw = '?'";
+		
+		PreparedStatement ps = TempSingleton.getConnection().prepareStatement(sql);
+	}
+	
+	
+	
+	
+	
+	
+	
+	// 지혁님
+	// 회원가입 기능 구현
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
 }
--- jdbc_project1/src/miniProject/TempSingleton.java
+++ jdbc_project1/src/miniProject/TempSingleton.java
@@ -9,12 +9,6 @@ public class TempSingleton {
 	private static Connection con = null;
 	private static Scanner sc = null;
 
-	private String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 커넥션 객체 생성을 위한 것을 만드는것.
-	// user name 기타 등등이 있습니다.
-	private String user = "LMS";
-	private String pwd = "1111";
-	private String driver = "oracle.jdbc.driver.OracleDriver";
-
 	// 접근 불가능한 기본생성자
 	private TempSingleton() {
 		// 접근 불가능한 코드
@@ -22,16 +16,19 @@ public class TempSingleton {
 
 	{
 		try {
-			Class.forName(driver);
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/** Connection 객체를 반환합니다. */
-	public Connection getConnection() {
+	public static Connection getConnection() {
 		if (con == null) {
 			try {
+				String user = "LMS";
+				String pwd = "1111";
+				String url = "jdbc:oracle:thin:@localhost:1521:xe";
 				con = DriverManager.getConnection(url, user, pwd);
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -42,7 +39,7 @@ public class TempSingleton {
 	}
 
 	/** Scanner 객체를 반환합니다. */
-	public Scanner getScanner() {
+	public static Scanner getScanner() {
 		if (sc == null) {
 			sc = new Scanner(System.in);
 		}
@@ -51,7 +48,7 @@ public class TempSingleton {
 	}
 
 	/** Connection 객체를 닫아줍니다. */
-	public void CloseConnection() {
+	public static void CloseConnection() {
 		try {
 			if (con != null) {
 				if (!con.isClosed()) {
