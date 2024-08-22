5ad1f7f3d2655c823c6af40340a831e1cd4b09e9
sks05194 <sks05194@gmail.com>
2024-08-22 18:20:16 +0900

    0822 naver join fixed

--- frontend/hw/naver_join/naver_join.js
+++ frontend/hw/naver_join/naver_join.js
@@ -13,7 +13,7 @@ $(".div_i > input[password]").on("blur", function () {
 // TODO: 생년월일 blur시 형식 확인 후
 // 살릴지 죽일까 생각할지 생각하겠다는 코드 작성 예정
 
-// 통신사 선택 버?튼 클릭 시 테이블 활성화/비활성화
+// 통신사 선택 클릭 시 테이블 활성화/비활성화
 $("#uagency").on("click", function () {
 	if ($("#uagency_div").is(':visible')) {
 		$("#uagency_div").hide();
@@ -24,13 +24,7 @@ $("#uagency").on("click", function () {
 	}
 });
 
-// TODO: 여기에 jquery문을 사용해서 통신사 선택 시
-// label의 border에 영행이 가도록 코드를 작성해주세요.
-
-// 성별, 국적 input:radio에 따라 label의 border 변경
-$("#rd_gr_div > .radio_group > input[type=radio]").on("change", function () {
-	// TODO: 졸리다. 위에 설명대로 잘 하세요.
-});
+// TODO: 통신서 선택 시 주는 영향에 대해 서술하시오.(3점)
 
 // TODO: 휴대전화번호 blur시 형식 변환
 // 형식 확인 먼저 하고 바꿔줄지 죽일지 선택.
